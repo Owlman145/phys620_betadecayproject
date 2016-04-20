caff8d7794bf78f74c116c11f7c2728b0b599d2b
felleg <felleg@users.noreply.github.com>
2016-04-20 15:48:49 -0400

    Major update, beta spectrum now fully functionnal.
    Beta spectrum is saved in a rootfile automatically. I created a function for the semi-empirical mass formula, but it is too imprecise for this simulation. I left it on the side for now.
    
    Parameters that can be changed by users are located between lines 21 and 34.

--- simulation/bdecay.cpp
+++ simulation/bdecay.cpp
@@ -12,119 +12,120 @@
 
 // ROOT libs
 #include<TH1D.h>
+#include<TFile.h>
 #include<TMath.h>
 #include<TRandom3>
 
 using namespace std;
 
+////////////////// Parameters ///////////////////////
+// Initial nucleus
+const int Z_1 = 6;	// Atomic number of initial nucleus
+const double m_1 = 14.003241;	// Isotope mass (in atomic mass units) 
+
+// Final nucleus
+const int Z_2 = 7;	// Atomic number of final nucleus
+const double m_2 = 14.0030740052;	// Isotope mass (in atomic mass  units)
+
+// Other parameters
+const int nevents = 1e6;// Number of events to generate
+const float h = 10;	// Constant used for Von Neuman method. Should range about [1,10]. Too low => cutting distribution, Too high => execution takes too long. Used to estimate the maximum of N(T_e)
+int ndivisions = 50;	// Number of divisions in energy histograms
+////////////////// End Of Parameters ///////////////
+
+// Physical Constants
+const double Pi = 3.14159265;	// Pi
+const double alpha = 1./137;	// Structure constant
+const float m_p = 938.272046;	// Mass (in MeV) of proton. From Wikipedia.
+const float m_n = 939.5654133;	// Mass (in MeV) of neutron. From Wikipedia.
+const float m_e = 0.511;	// Mass (in MeV) of electron. From Wikipedia.
+const float m_nu= 0.2e-6;	// Mass (in MEV) of neutrino. Value was given in our project synopsis
+
+// Functions
+float N(float, int, float);		// Distribution of energy, N(T_e)
+float F(int, float, int);	// Fermi function, F(Z',T_e)
+float m(float, float);	// Semi-empirical mass formula (Bertulani eq 5.8)
+
+// Main program
 void bdecay(){
+	// Parameter-dependent values
+	const int charge = Z_1 - Z_2;		// Charge of electron/positron (in units of e)
+	float Q;	// Q-value of our experiment
+	if (charge == -1)	// If negative beta decay...
+	{
+		Q = 931.5*(m_1-m_2);	// Taken from Bertulani eq 8.9. The 931.5 factor is to convert amu unit to MeV unit.
+	}
+	else if (charge == +1)	// If positive beta decay...
+	{
+		Q = 931.5*(m_1-m_2)-2m_e;	// Taken from Bertulani eq 8.9. The 931.5 factor is to convert amu unit to MeV unit.
+	}
+	else	// This is not a beta decay
+	{
+		break;
+	}
+
 	gStyle->SetOptStat("nemr");	// Makes statistics box appear automatically in histograms
 
-	// Simulation parameters
+	// ROOT random number generator
 	TRandom3 *rand = new TRandom3();	// Generate a random number generator for TRandom3
-	const int nevents=5e6;			// Number of events to generate
-	const double Pi = 3.14159265;	// Pi
-	int ndivisions = 40;		// Number of divisions in energy histograms
-
-	float m_A = 14.00341*931.5;	// Mass (in MeV) of initial particle at rest.
-	float m_B = 14.00307*931.5;	// Mass (in MeV) of final particle. 
-	float Q= m_A-m_B;	// Q-value of our experiment (Taken from Krane, eq. 9.6)
-	float m_e = 0.511;	// Mass (in MeV) of electron
-	float m_nu= 0.2e-6;	// Mass (in MEV) of neutrino
 
 	// ROOT Histograms
-	TH1D *p_p = new TH1D("p_p", ";p_{p} [MeV];Intensity", ndivisions, 0, Q);	// Momentum histogram for proton
-	TH1D *p_e = new TH1D("p_e", ";p_{e} [MeV];Intensity", ndivisions, 0, Q);	// Momentum histogram for electron
-	TH1D *p_nu = new TH1D("p_nu",";p_{#nu_{e}} [MeV];Intensity", ndivisions, 0, Q);	// Momentum histogram for neutrino
-	TH1D *p_total = new TH1D("p_total", ";p_{total} [MeV];Intensity", ndivisions, 0.5*Q, 1.5*Q);	// Total momentum histogram
+	TH1D *E_e = new TH1D("E_e", ";E_{e} [MeV];Intensity", ndivisions, 0, 1.1*Q);	// Energy histogram for electron
 
-	TH1D *E_p = new TH1D("E_p", ";E_{p} [MeV];Intensity", ndivisions, 0, Q);	// Energy histogram for proton
-	TH1D *E_e = new TH1D("E_e", ";E_{e} [MeV];Intensity", ndivisions, 0, Q);	// Energy histogram for electron
-	TH1D *E_nu = new TH1D("E_nu",";E_{#nu_{e}} [MeV];Intensity", ndivisions, 0, Q);	// Energy histogram for neutrino
-	TH1D *E_total = new TH1D("E_total", ";E_{total} [MeV];Intensity", ndivisions, 0.5*Q, 1.5*Q);	// Total energy histogram
+	// ROOT rootfile (will contain all histograms)
+	TFile *rootfile = new TFile("beta_decay_histograms.root", "recreate");
+
+	cout << "(Generating 1e" << log10(1.*nevents) << " events...)\n";
+	cout << "Q = " << Q << " MeV\n";
 
 	int counter=0;	// Counter for the while loop
-	cout << "(Total: 1e" << log10(1.*nevents) << " events)\n";
-	cout << "Q = " << Q << " MeV\n";
 	while (counter < nevents) {
-		//double r1 = rand->Uniform(1);	// Number between 0 and 1
-		double r1 = 0;	// We choose it to be 0, as we want to neglect the recoil energy of the nucleus 
-		double r2 = rand->Uniform(1);	// Number between 0 and 1
-		if (r1+r2 <= 1) {	// We make sure that r1+r2 is not too big so that p_nu is not negative
-			// Atribute randomly generated energy to each particle
-			float K1 = r1*Q;	// Kinetic energy of proton (we neglect recoil)
-			float K2 = r2*Q;	// Kinetic energy of electron
-			float K3 = Q*(1-r1-r2);	// Kinetic energy of neutrino
+		// We use Von Neumann acceptance-rejection method, see phys620 course notes (Monte Carlo p. 21)
+		double T_e = rand->Uniform(Q);	// Number between 0 and Q, as there is no energy above Q
+		double u = rand->Uniform(1);	// Number between 0 and 1
+		if (u <= N(T_e, charge, Q) / (h*N(Q/2, charge, Q)))	// "h" is a factor that can be changed so that the sample is more efficient. See phys620 course notes (Monte Carlo p. 21)
+		{
+			E_e->Fill(T_e);	// Enter electron kinetic energy in histogram to create beta decay spectrum
 
-			float p1 = sqrt(2*K1*m_B);	// proton momentum
-			float p2 = sqrt(2*K2*m_e);	// electron momentum
-			float p3 = sqrt(2*K3*m_nu);	// neutrino momentum
-
-			//double angle1 = rand->Uniform(0,2*Pi);	// Get random angle in radians for proton
-			//double angle2 = -1;	// I would like to write an expression that uses conservation of momentum to find angle2 from angle1 and angle3
-			//double angle3 = rand->Uniform(0,2*Pi);	// Get random angle in radians for neutrino
-
-			//float E1 = sqrt(pow(m_B,2)+pow(p1,2));	// proton
-			//float E3 = sqrt(pow(m_nu,2)+pow(p3,2));	// neutrino
-			//float E2 = 1./(2*m_A)*(pow(m_A,2) + pow(m_e,2) - pow(m_B,2) - pow(m_nu,2) - 2*E1*E3 + 2*p1*p3*cos(abs(angle1-angle3)));	//electron, using quadri-impulsion conservation (particle A supposed at rest)
-
-			p_p->Fill(p1);	// Fill histogram p_p using momentum p1
-			p_e->Fill(p2);	// Fill histogram p_e using momentum p2
-			p_nu->Fill(p3);	// Fill histogram p_nu using momentum p3
-			p_total->Fill(p1+p2+p3);	// Fill histogram of total momentum
-
-			E_p->Fill(K1);	// Fill histogram E_p using energy E1
-			E_e->Fill(K2);	// Fill histogram E_p using energy E2
-			E_nu->Fill(K3);	// Fill histogram E_p using energy E3
-			E_total->Fill(K1+K2+K3);	// Fill histogram of total energy
-
-			
 			// For execution purposes, acts as a "progress bar"
 			if (!(++counter % (nevents/10))) {	// Add 1 to counter and take its modulo
-				cout << "Current progress: " << 100.*counter/nevents << "%"<< endl;
+				cout << "Current progress: " << 100.*counter/nevents << "%"<< endl;	// Display progress
 			}
 		}
 	}
 
-	//TCanvas *c0=new TCanvas("p_total","p_total");
-	//p_total->SetFillColor(6);	//magenta
-	//p_total->Draw();
-
-	TCanvas *c1=new TCanvas("p_p", "p_p");
-	p_p->SetFillColor(2);	//red
-	p_p->Draw();
-
-	TCanvas *c2=new TCanvas("p_e","p_e");
-	p_e->SetFillColor(4);	//blue
-	p_e->Draw();
-
-	TCanvas *c3=new TCanvas("p_nu","p_nu");
-	p_nu->SetFillColor(3);	//green
-	p_nu->Draw();
-
-//	TCanvas *c4=new TCanvas("All_momentum_spectrums","All_momentum_spectrums");
-//	p_p->Draw("");
-//	p_e->Draw("same");
-//	p_nu->Draw("same");
-
-	TCanvas *c00=new TCanvas("E_total","E_total");
-	E_total->SetFillColor(8);	//ugly green
-	E_total->Draw();
-
-	TCanvas *c5=new TCanvas("E_p", "E_p");
-	E_p->SetFillColor(2);	//red
-	E_p->Draw();
-
-	TCanvas *c6=new TCanvas("E_e","E_e");
+	E_e->Write();	// Save histogram into the rootfile
+	TCanvas *c1=new TCanvas("E_e","E_e");	// ROOT canvas creation
 	E_e->SetFillColor(4);	//blue
-	E_e->Draw();
+	E_e->Draw();	// Draw histogram
+}
 
-	TCanvas *c7=new TCanvas("E_nu","E_nu");
-	E_nu->SetFillColor(3);	//green
-	E_nu->Draw();
+// Energy distribution for beta decay
+float N(float T_e, int charge, float Q)
+{
+	return sqrt( pow(T_e,2) + 2*T_e*m_e ) * (T_e + m_e) * (Q-T_e) * sqrt( pow(Q-T_e,2) - pow(m_nu,2) ) * F(Z_2,T_e, charge); // Supposing C=1, taken from http://www2.warwick.ac.uk/fac/sci/physics/research/epp/exp/detrd/amber/betaspectrum/ 
+}
 
-//	TCanvas *c8=new TCanvas("All_energy_spectrums","All_energy_spectrums");
-//	E_p->Draw("");
-//	E_e->Draw("same");
-//	E_nu->Draw("same");
+// Fermi function
+float F(int Z_2, float T_e, int charge)
+{
+	float eta = (T_e + m_e) * charge * alpha * Z_1 / sqrt(2*T_e*m_e);	// Taken from https://en.wikipedia.org/wiki/Beta_decay#Fermi_function
+	return 2. * Pi * eta / (1 - exp(-2*Pi*eta)); // Taken from https://en.wikipedia.org/wiki/Beta_decay#Fermi_function
+}
+
+// Semi-empirical mass formula (Bertulani eq 5.8). Returns mass in MeV
+float m(float Z, float A)
+{
+	// Constants (in MeV) for binding energy B. Taken from Bertulani, eq 5.9
+	float a_v = 15.85;
+	float a_s = 18.34;
+	float a_c = 0.71;
+	float a_a = 92.86;
+	float a_p = 1.46;
+
+	// Binding energy formula, taken from Bertulani eq 5.8
+	float B = a_v*A - a_s*pow(1.*A, 2./3.) - a_c*pow(Z,2)*pow(1.*A,-1./3.) - a_a*pow(Z-1.*A/2.,2)/A - 0.5*(pow(-1,Z)+pow(-1,A-Z))*a_p*pow(1.*A,-1./2);
+
+	return Z*m_p + (A-Z)*m_n - B;	// From Bertulani, eq 4.9
+	
 }
