1e3af6f1e9fa8ce1f1c5ecc1ef8cc65c02141fae
felleg <felleg@users.noreply.github.com>
2016-04-26 11:21:01 -0400

    Update bdecay2.cpp

--- simulation/bdecay2.cpp
+++ simulation/bdecay2.cpp
@@ -31,9 +31,6 @@ const double m_2 = 3.0160293;	// Isotope mass (in atomic mass  units)
 // Other parameters
 const int charge = -1;
 const float Q = 931.5e6*(m_1-m_2);
-//const int nevents = 1e7;// Number of events to generate
-//const float h = 3; // Constant used for Von Neuman method. Should range about [1,10]. Too low => cutting distribution, Too high => execution takes too long. Used to estimate the maximum of N(T_e)
-//float limit=0;	// Multiple of Q over which we necessitate the energy to be
 const int ndivisions = 100;	// Number of divisions in energy histograms
 ////////////////// End Of Parameters ///////////////
 
@@ -55,9 +52,6 @@ void bdecay2(){
 
 	gStyle->SetOptStat("nemr");	// Makes statistics box appear automatically in histograms
 
-	// ROOT random number generator
-	//TRandom3 *rand = new TRandom3(time(0));	// Generate a random number generator for TRandom3
-
 	cout << "Enter the name of the rootfile you will load (e.g. b_decay_histo.root): ";
 	string filename;
 	cin >> filename;
@@ -75,27 +69,6 @@ void bdecay2(){
 	func->SetParName(1,"C");
 	func->SetLineColor(2);	// red
 
-	//cout << "(Generating 1e" << log10(1.*nevents) << " events...)\n";
-	//cout << "Q = " << Q << " eV\n";
-
-//	int counter=0;	// Counter for the while loop
-//	while (counter < nevents) {
-//		// We use Von Neumann acceptance-rejection method, see phys620 course notes (Monte Carlo p. 21)
-//		double T_e = rand->Uniform(limit,Q);	// Number between 0 and Q, as there is no energy above Q
-//
-//		double u = rand->Uniform(1);	// Number between 0 and 1
-//		if (u <= N(T_e, m_nu, 1.) / (h*N(Q/2, m_nu, 1)))	// "h" is a factor that can be changed so that the sample is more efficient. See phys620 course notes (Monte Carlo p. 21)
-//		{
-//			E_e->Fill(T_e);	// Enter electron kinetic energy in histogram to create beta decay spectrum
-//
-//			// For execution purposes, acts as a "progress bar"
-//			if (!(++counter % (nevents/10))) {	// Add 1 to counter and take its modulo
-//				cout << "Current progress: " << 100.*counter/nevents << "%"<< endl;	// Display progress
-//			}
-//		}
-//	}
-//
-//	E_e->Write();	// Save histogram into the rootfile
 	TCanvas *c1=new TCanvas("E_e","E_e");	// ROOT canvas creation
 	E_e->Fit("func","");
 	E_e->SetFillColor(4);	//blue
