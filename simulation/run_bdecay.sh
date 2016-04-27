c010f860b24cb7ccf7695383f37f78174fa67e9d
felleg <felleg@users.noreply.github.com>
2016-04-27 11:06:18 -0400

    Update run_bdecay.sh

--- simulation/run_bdecay.sh
+++ simulation/run_bdecay.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+#chmod 755 run_bdecay.sh (do this only ONCE to make the file executable)
+#./run_bdecay.sh (do this to execute this file)
+
 echo "Enter the name of the rootfile you will output WITHOUT '.root' (e.g. b_decay_histo): "
 read filename
 root -l -q 'bdecay.cpp("'$filename'")'
