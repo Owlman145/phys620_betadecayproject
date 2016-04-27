9f640a2a166394333b41d3e452dad97e4a6049a4
felleg <felleg@users.noreply.github.com>
2016-04-27 11:03:07 -0400

    Update bdecay.cpp

--- simulation/bdecay.cpp
+++ simulation/bdecay.cpp
@@ -30,9 +30,10 @@ const double m_2 = 3.0160293;	// Isotope mass (in atomic mass  units)
 // Other parameters
 const int charge = -1;
 const float Q = 931.5e6*(m_1-m_2);
-const int nevents = 1e6;// Number of events to generate
-const float h = 0.001; // Constant used for Von Neuman method. Should range about [1,10]. Too low => cutting distribution, Too high => execution takes too long. Used to estimate the maximum of N(T_e)
-float limit=0.99;	// Multiple of Q over which we necessitate the energy to be
+const int nevents = 1e7;// Number of events to generate
+const float res = 395;	// Resolution of detector (in eV)
+const float h = 3;	// Constant used for Von Neuman method. Should range about [1,10]. Too low => cutting distribution, Too high => execution takes too long. Used to estimate the maximum of N(T_e)
+float limit=0;	// Number between 0 and 1. Fraction of Q over which we necessitate the energy to be 
 const int ndivisions = 100;	// Number of divisions in energy histograms
 ////////////////// End Of Parameters ///////////////
 
@@ -49,21 +50,18 @@ float N(float, float, float);		// Distribution of energy, N(T_e)
 float F(int, float, int);	// Fermi function, F(Z',T_e)
 
 // Main program
-void bdecay(){
-	limit= limit*Q;	// Limit above which we want our spectrum
+void bdecay(string filename){
 
-	gStyle->SetOptStat("nemr");	// Makes statistics box appear automatically in histograms
+	limit= limit*Q;	// Limit above which we want our spectrum
 
 	// ROOT random number generator
 	TRandom3 *rand = new TRandom3(time(0));	// Generate a random number generator for TRandom3
 
 	// ROOT Histograms
-	TH1D *E_e = new TH1D("E_{e}", ";E_{e} [eV];Intensity", ndivisions, limit, Q);	// Energy histogram for electron
+	TH1D *E_e = new TH1D("E_{e}", ";E_{e} [eV];Intensity", ndivisions, limit, Q);	// True kinetic energy histogram for electron
 	E_e->SetName("E_e");
-
-	cout << "Enter the name of the rootfile you will output (e.g. b_decay_histo): ";
-	string filename;
-	cin >> filename;
+	TH1D *E_e_sm = new TH1D("E_{e}", ";E_{e} [eV];Intensity", ndivisions, limit, Q);	// Smeared kinetic energy histogram for electron
+	E_e_sm->SetName("E_e_sm");
 
 	// ROOT rootfile (will contain all histograms)
 	TFile *rootfile = new TFile((filename + ".root").c_str(), "recreate");
@@ -74,12 +72,15 @@ void bdecay(){
 	int counter=0;	// Counter for the while loop
 	while (counter < nevents) {
 		// We use Von Neumann acceptance-rejection method, see phys620 course notes (Monte Carlo p. 21)
-		double T_e = rand->Uniform(limit,Q);	// Number between 0 and Q, as there is no energy above Q
+		double T_e = rand->Uniform(limit,Q);	// True kinetic energy of the electron. Number between 0 and Q, as there is no energy above Q
 
 		double u = rand->Uniform(1);	// Number between 0 and 1
 		if (u <= N(T_e, m_nu, 1.) / (h*N(Q/2, m_nu, 1)))	// "h" is a factor that can be changed so that the sample is more efficient. See phys620 course notes (Monte Carlo p. 21)
 		{
-			E_e->Fill(T_e);	// Enter electron kinetic energy in histogram to create beta decay spectrum
+			E_e->Fill(T_e);		// Enter true electron kinetic energy in histogram to create beta decay spectrum
+
+			double T_e_sm = rand->Gaus(T_e,res);	// Smeared kinetic energy
+			E_e_sm->Fill(T_e_sm);	// Enter smeared electron kinetic energy in histogram to create beta decay spectrum
 
 			// For execution purposes, acts as a "progress bar"
 			if (!(++counter % (nevents/10))) {	// Add 1 to counter and take its modulo, if we finished a 10% of the job
@@ -88,17 +89,10 @@ void bdecay(){
 		}
 	}
 	E_e->Write();	// Save histogram into the rootfile
+	E_e_sm->Write();	// Save histogram into the rootfile
 }
 
 // Energy distribution for beta decay
 float N(float T_e, float m_nu, float C)
 {
-	return C*sqrt( pow(T_e,2) + 2*T_e*m_e ) * (T_e + m_e) * (Q-T_e) * sqrt( pow(Q-T_e,2) - pow(m_nu,2) ) * F(Z_2,T_e, charge); // Supposing C=1, taken from http://www2.warwick.ac.uk/fac/sci/physics/research/epp/exp/detrd/amber/betaspectrum/ 
-}
-
-// Fermi function
-float F(int Z_2, float T_e, int charge)
-{
-	float eta = (T_e + m_e) * charge * alpha * Z_1 / sqrt(2*T_e*m_e);	// Taken from https://en.wikipedia.org/wiki/Beta_decay#Fermi_function
-	return 2. * Pi * eta / (1 - exp(-2*Pi*eta)); // Taken from https://en.wikipedia.org/wiki/Beta_decay#Fermi_function
-}
+	return C*sqrt( pow(T_e,2) + 2*T_e*m_e ) * (T_e + m_e) * (Q-T_e) * sqrt( pow(Q-T_e,2) - pow(m_nu,2) ) * F(Z_2,T_e, charge); // Supposing C=1, taken from http://www2.warwick.ac.uk/fac/sci/physics/research/epp/exp/detrd/amber/betas
